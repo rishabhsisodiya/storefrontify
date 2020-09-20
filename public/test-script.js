b4e9306ece5e174c1087f842b3573e74ac2a10c7
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-21 04:39:26 +0530

    script tag store front

--- public/test-script.js
+++ public/test-script.js
@@ -1,1 +1,6 @@
-console.log('test script');
+console.log("test script");
+
+const header = $("header-site-header").parent();
+header
+  .prepend("<div>hello coming from test script in public folder</div>")
+  .css({'background-color':'black','text-sign':'center' });
