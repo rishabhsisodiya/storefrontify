a61667e377a51adba5984742ef1f4dd99405f7e3
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-21 04:57:50 +0530

    redeclaration header error

--- public/test-script.js
+++ public/test-script.js
@@ -1,6 +1,6 @@
 console.log("test script");
 
-const headerElement = $("header.site-header").parent();
-headerElement
+const header = $("shopify-section-header").parent();
+header
   .prepend("<div>hello coming from test script in public folder</div>")
   .css({'background-color':'black','text-sign':'center' });
