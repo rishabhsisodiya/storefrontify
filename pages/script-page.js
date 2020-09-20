a4d77042c726d7172d7cf95458cd0f8fd5917e78
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-21 04:22:59 +0530

    update .env

--- pages/script-page.js
+++ pages/script-page.js
@@ -1,4 +1,24 @@
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+
+const CREATE_SCRIPT_TAG = gql`
+    mutation scriptTagCreate($input: ScriptTagInput!) {
+        scriptTagCreate(input: $input) {
+            scriptTag {
+                id
+            }
+            userErrors {
+                field
+                message
+            }
+        }
+    }
+`;
+
+
 const ScriptPage = () => {
+    const [createScripts] = useMutation(CREATE_SCRIPT_TAG);
     return (
         <div>
             <p>Hello From Script Page</p>
