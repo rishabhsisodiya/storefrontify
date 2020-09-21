75a485e019b03f086c733d8b67527f0ed89e447e
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-21 07:49:50 +0530

    add modal

--- public/test-script.js
+++ public/test-script.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import PopOver from './PopOver';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import PopOver from './PopOver';
 
-// const header = $("header.scrollheader").parent();
-// header
-//   .prepend("<div id='sample'>hello coming from test script in public folder</div>")
-//   .css({'background-color':'orange','text-sign':'center' });
+const header = $("header.scrollheader").parent();
+header
+  .prepend("<div>hello coming from test script in public folder</div>")
+  .css({'background-color':'orange','text-sign':'center' });
 
-ReactDOM.render(
-  <React.StrictMode>
-    <PopOver/>
-  </React.StrictMode>,
-  document.getElementById('banner-message')
-);
+  document.body.appendChild("<div>REACT COMPONENT</div>")
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <PopOver/>
+//   </React.StrictMode>,
+//   document.getElementById('banner-message')
+// );
