46d49cd2ab4ebc06f7d2819838f4f1e7e00c4248
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-21 20:23:09 +0530

    add create or delete tags

--- pages/script-page.js
+++ pages/script-page.js
@@ -1,63 +1,127 @@
-import gql from "graphql-tag";
-import { useQuery, useMutation } from "@apollo/react-hooks";
-import { Spinner } from "@shopify/polaris";
+import gql from 'graphql-tag';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { Button, Card, Layout, Page, ResourceList, Stack, Spinner } from '@shopify/polaris';
 
 const CREATE_SCRIPT_TAG = gql`
-  mutation scriptTagCreate($input: ScriptTagInput!) {
-    scriptTagCreate(input: $input) {
-      scriptTag {
-        id
-      }
-      userErrors {
-        field
-        message
-      }
+    mutation scriptTagCreate($input: ScriptTagInput!) {
+        scriptTagCreate(input: $input) {
+            scriptTag {
+                id
+            }
+            userErrors {
+                field
+                message
+            }
+        }
     }
-  }
 `;
 
 const QUERY_SCRIPTTAGS = gql`
-  query {
-    scriptTags(first: 5) {
-      edges {
-        node {
-          id
-          src
-          displayScope
+    query {
+        scriptTags(first: 5) {
+            edges {
+                node {
+                    id
+                    src
+                    displayScope
+                }
+            }
         }
-      }
     }
-  }
 `;
 
 const DELETE_SCRIPTTAG = gql`
-  mutation scriptTagDelete($id: ID!) {
-    scriptTagDelete(id: $id) {
-      deletedScriptTagId
-      userErrors {
-        field
-        message
-      }
+    mutation scriptTagDelete($id: ID!) {
+        scriptTagDelete(id: $id) {
+            deletedScriptTagId
+            userErrors {
+                field
+                message
+            }
+        }
     }
-  }
 `;
 
-const ScriptPage = () => {
-  // const [createScripts] = useMutation(CREATE_SCRIPT_TAG);
+function ScriptPage() {
+
+  const [createScripts] = useMutation(CREATE_SCRIPT_TAG);
+  const [deleteScripts] = useMutation(DELETE_SCRIPTTAG);
   const { loading, error, data } = useQuery(QUERY_SCRIPTTAGS);
-  if (loading) return <Spinner accessibilityLabel="Query Spinner" />;
+
+
+  if (loading) return <Spinner accessibilityLabel="Query loading" />;
   if (error) return <div>{error.message}</div>;
-  console.log(data);
+
   return (
-    <div>
-      <p>These are available script tags</p>
-      {data.scriptTags.edges.map((item) => (
-          <div key={item.node.id}>
-              <p>{item.node.id}</p>
-          </div>
-      ) )}
-    </div>
-  );
-};
+    <Page>
+      <Layout>
+        <Layout.Section>
+          <Card title="These are the Script Tags:" sectioned>
+            <p>
+              Create or Delete a Script Tag
+            </p>
+          </Card>
+        </Layout.Section>
+        <Layout.Section secondary>
+          <Card title="Script Tag" sectioned>
+            <Button
+              primary
+              size="slim"
+              type="submit" onClick={() => {
+                createScripts({
+                  variables: {
+                    input: {
+                      src: "https://d733e9bf.ngrok.io/test-script.js",
+                      displayScope: "ALL"
+                    }
+                  },
+                  refetchQueries: [{ query: QUERY_SCRIPTTAGS }]
+                })
+              }}
+            >
+              Create Script Tag
+            </Button>
+          </Card>
+        </Layout.Section>
+        <Layout.Section>
+          <Card>
+            <ResourceList
+              showHeader
+              resourceName={{ singular: 'Script', plural: 'Scripts' }}
+              items={data.scriptTags.edges}
+              renderItem={item => {
+                return (
+                  <ResourceList.Item
+                    id={item.id}
+                  >
+                    <Stack>
+                      <Stack.Item>
+                        <p>
+                          {item.node.id}
+                        </p>
+                      </Stack.Item>
+                      <Stack.Item>
+                        <Button type='submit' onClick={() => {
+                          deleteScripts({
+                            variables: {
+                              id: item.node.id
+                            },
+                            refetchQueries: [{ query: QUERY_SCRIPTTAGS }]
+                          })
+                        }}>
+                          Delete Script Tag
+                        </Button>
+                      </Stack.Item>
+                    </Stack>
+                  </ResourceList.Item>
+                )
+              }}
+            />
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  )
+}
 
 export default ScriptPage;
