93919e2596a2fd00bccf93b4754dc66211219c07
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-21 16:12:55 +0530

    Run REact

--- public/test-script.js
+++ public/test-script.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PopOver from './PopOver';
 
-// const header = $("header.scrollheader").parent();
-// header
-//   .prepend("<div id='sample'>hello coming from test script in public folder</div>")
-//   .css({'background-color':'orange','text-sign':'center' });
+const header = $("header.scrollheader").parent();
+header
+  .prepend("<div id='sample'>hello coming from test script in public folder</div>")
+  .css({'background-color':'orange','text-sign':'center' });
 
 // const divElement= document.body.appendChild("<div>REACT COMPONENT</div>")
 // const preEpem=document.body.prepend('<div>REACT COMPONENT</div>')
@@ -18,5 +18,5 @@ import PopOver from './PopOver';
 // const bodyEl=document.getElementsByTagName('body');
 // bodyEl.append('<div>Hello from React</dov>')
 // console.log(bodyEl);
-
+console.log(ReactDOM);
 ReactDOM.render(<div>hello React</div>, document.getElementById("PageContainer"));
