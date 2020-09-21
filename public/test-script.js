12e9959f0e2a1a380997f2b256016eed5f659b08
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-21 15:54:07 +0530

    Run REact

--- public/test-script.js
+++ public/test-script.js
@@ -1,11 +1,11 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import PopOver from './PopOver';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PopOver from './PopOver';
 
-const header = $("header.scrollheader").parent();
-header
-  .prepend("<div id='sample'>hello coming from test script in public folder</div>")
-  .css({'background-color':'orange','text-sign':'center' });
+// const header = $("header.scrollheader").parent();
+// header
+//   .prepend("<div id='sample'>hello coming from test script in public folder</div>")
+//   .css({'background-color':'orange','text-sign':'center' });
 
 // const divElement= document.body.appendChild("<div>REACT COMPONENT</div>")
 // const preEpem=document.body.prepend('<div>REACT COMPONENT</div>')
@@ -15,6 +15,8 @@ header
 //   </React.StrictMode>,
 //   document.getElementById('banner-message')
 // );
-const bodyEl=document.getElementsByTagName('body');
-bodyEl.append('<div>Hello from React</dov>')
+// const bodyEl=document.getElementsByTagName('body');
+// bodyEl.append('<div>Hello from React</dov>')
 // console.log(bodyEl);
+
+ReactDOM.render('<div>hello React</div>', document.body.childNodes[1]);
