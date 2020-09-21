d0229bf6200faf774f4c8e4782019e5de43d16e4
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-22 00:52:58 +0530

    storefront best seller button

--- public/test-script.js
+++ public/test-script.js
@@ -1,6 +1,73 @@
-const header = $("header.scrollheader").parent();
-header
-  .prepend(
-    "<div id='sample'>hello coming from test script in public folder</div>"
-  )
-  .css({ "background-color": "orange", "text-sign": "center" });
+// const header = $("header.scrollheader").parent();
+// header
+//   .prepend(
+//     "<div id='sample'>hello coming from test script in public folder</div>"
+//   )
+//   .css({ "background-color": "orange", "text-sign": "center" });
+
+
+const body = $('body');
+
+body.css({
+    'position': 'relative'
+})
+
+const shop = Shopify.shop;
+
+
+const makeApp = products => {
+
+    const bestSellerContainer = $(
+        `<div style="overflow-y: scroll;">
+            <h3>Our Best Sellers</h3>
+            ${products.map(item => {
+                return `
+                <a href="/products/${item.handle}" style="display: flex; align-items: center; padding: 20px 10px; border-top: 1px solid black;">
+                    <img src=${item.images[0].originalSrc} style="width: 75px;" />
+                    <div style="display: flex; justify-content: space-between; align-items: start; width: 100%;">
+                        <p style="padding: 0 10px;">${item.title}</p>
+                        <p>${item.variants[0].price}</p>
+                    </div>
+                </a>
+                `
+            }).join('')
+        }
+        </div>`
+    )
+    .css({
+        'position': 'fixed',
+        'background-color': '#ffffff',
+        'padding': '10px',
+        'border': '1px solid black',
+        'bottom': '80px',
+        'right': '25px',
+        'height': '400px',
+        'width': '350px',
+        'display': 'none'
+    })
+
+    const bestSellerButton = $('<img />').attr('src', 'https://cdn.shopify.com/s/files/1/0325/3174/2765/files/bestseller-button-trans.png?v=1584741923')
+        .css({
+            'position': 'fixed',
+            'width': '150px',
+            'bottom': '20px',
+            'right': '20px',
+            'cursor': 'pointer'
+        })
+
+        body.append(bestSellerButton);
+        body.append(bestSellerContainer);
+
+        bestSellerButton.click(() => {
+            bestSellerContainer.slideToggle();
+        })
+}
+// https://storefrontify.herokuapp.com/auth?shop=ambraee-dev1.myshopify.com
+// for cors issue add https://cors-anywhere.herokuapp.com/ below with https://storefrontify.herokuapp.com
+fetch('https://storefrontify.herokuapp.com/api/products?shop=ambraee-dev1.myshopify.com')
+    .then(res => res.json())
+    .then(data => {
+        makeApp(data.data)
+        console.log(data)
+    })
+    .catch(error => console.log(error))
