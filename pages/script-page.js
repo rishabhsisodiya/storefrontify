1d53f1a43e5b1365bf1ede0592aee824ad7c9314
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-21 19:43:24 +0530

    display all script tags

--- pages/script-page.js
+++ pages/script-page.js
@@ -1,55 +1,62 @@
-import gql from 'graphql-tag';
-import { useQuery, useMutation } from '@apollo/react-hooks';
-
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import { Spinner } from "@shopify/polaris";
 
 const CREATE_SCRIPT_TAG = gql`
-    mutation scriptTagCreate($input: ScriptTagInput!) {
-        scriptTagCreate(input: $input) {
-            scriptTag {
-                id
-            }
-            userErrors {
-                field
-                message
-            }
-        }
+  mutation scriptTagCreate($input: ScriptTagInput!) {
+    scriptTagCreate(input: $input) {
+      scriptTag {
+        id
+      }
+      userErrors {
+        field
+        message
+      }
     }
+  }
 `;
 
 const QUERY_SCRIPTTAGS = gql`
-    query {
-        scriptTags(first: 5) {
-            edges {
-                node {
-                    id
-                    src
-                    displayScope
-                }
-            }
+  query {
+    scriptTags(first: 5) {
+      edges {
+        node {
+          id
+          src
+          displayScope
         }
+      }
     }
+  }
 `;
 
 const DELETE_SCRIPTTAG = gql`
-    mutation scriptTagDelete($id: ID!) {
-        scriptTagDelete(id: $id) {
-            deletedScriptTagId
-            userErrors {
-                field
-                message
-            }
-        }
+  mutation scriptTagDelete($id: ID!) {
+    scriptTagDelete(id: $id) {
+      deletedScriptTagId
+      userErrors {
+        field
+        message
+      }
     }
+  }
 `;
 
-
 const ScriptPage = () => {
-    const [createScripts] = useMutation(CREATE_SCRIPT_TAG);
-    return (
-        <div>
-            <p>Hello From Script Page</p>
-        </div>
-    )
-}
+  // const [createScripts] = useMutation(CREATE_SCRIPT_TAG);
+  const { loading, error, data } = useQuery(QUERY_SCRIPTTAGS);
+  if (loading) return <Spinner accessibilityLabel="Query Spinner" />;
+  if (error) return <div>{error.message}</div>;
+  return (
+    <div>
+      <p>These are available script tags</p>
+      {data.scriptTags.edges.map((item) => (
+          <div key={item.node.id}>
+              <p>{item.node.id}</p>
+          </div>
+      ) )}
+    </div>
+  );
+};
 
 export default ScriptPage;
--- public/test-script.js
+++ public/test-script.js
@@ -1,52 +1,6 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import PopOver from './PopOver';
-
 const header = $("header.scrollheader").parent();
 header
   .prepend(
     "<div id='sample'>hello coming from test script in public folder</div>"
   )
   .css({ "background-color": "orange", "text-sign": "center" });
-  var reactScript = document.createElement("script");
-  reactScript.setAttribute("src","https://unpkg.com/react@16/umd/react.development.js")
-  var reactDomScript = document.createElement("script");
-  reactDomScript.setAttribute("src","https://unpkg.com/react-dom@16/umd/react-dom.development.js")
-  // var myScript = document.createElement("script");
-  // myScript.setAttribute("src","Hello.js")
-  document.body.appendChild(reactScript);
-  document.body.appendChild(reactDomScript);
-  ReactDOM.render('<div>React Magic</div>', document.getElementById('sample'));
-  // document.body.appendChild(myScript);
-
-// var node = document.createElement("h1");
-// var textnode = document.createTextNode("Water");
-// node.appendChild(textnode);
-// document.body.appendChild(node);
-{/* <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script> */}
-{/* <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script> */}
-
-// node.setAttribute("crossorigin")
-// const element = React.createElement(
-//   "h1",
-//   { className: "greeting" },
-//   "Hello, world!"
-// );
-// var node = React.createElement("h1", "Hello from React");
-// var textnode = document.createTextNode("Water");
-// node.appendChild(textnode);
-
-// document
-// const divElement= document.body.appendChild("<div>REACT COMPONENT</div>")
-// const preEpem=document.body.prepend('<div>REACT COMPONENT</div>')
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <PopOver/>
-//   </React.StrictMode>,
-//   document.getElementById('banner-message')
-// );
-// const bodyEl=document.getElementsByTagName('body');
-// bodyEl.append('<div>Hello from React</dov>')
-// console.log(bodyEl);
-// console.log(ReactDOM);
-// ReactDOM.render(<div>hello React</div>, document.getElementById("PageContainer"));
