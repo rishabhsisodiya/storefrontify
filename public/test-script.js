80cc0496a227ebc7a3d2fd9171b371671857ff0e
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-22 16:44:53 +0530

    add both button

--- public/test-script.js
+++ public/test-script.js
@@ -18,6 +18,36 @@ body.css({
 
 const shop = Shopify.shop;
 
+const bestSellerButton = $('<img/>').attr('src', 'https://cdn.shopify.com/s/files/1/0325/3174/2765/files/bestseller-button-trans.png?v=1584741923')
+.css({
+    'position': 'fixed',
+    'width': '150px',
+    'bottom': '20px',
+    'left': '20px',
+    'cursor': 'pointer',
+    'z-index':1,
+})
+
+const bestSellerContainer =  $(`<div>Our Best Seller</div>`).css({
+    'position': 'fixed',
+    'background-color': '#ffffff',
+    'padding': '10px',
+    'border': '1px solid black',
+    'bottom': '80px',
+    'left': '25px',
+    'height': '400px',
+    'width': '350px',
+    'display': 'none',
+    'z-index':1,
+});
+
+body.append(bestSellerButton);
+body.append(bestSellerContainer);
+
+bestSellerButton.click(() => {
+    bestSellerContainer.slideToggle();
+})
+
 const makeApp = (products) => {
 
     const bestSellerContainer = $(
