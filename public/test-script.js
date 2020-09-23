55e3180029530a5f27ac9806955dc3c241c02c11
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-23 19:49:01 +0530

    remove fetch

--- public/test-script.js
+++ public/test-script.js
@@ -107,7 +107,7 @@ $("#sendemailbutton").click(() => {
     alert($("#cEmail").val());
     $("#cEmail").val("");
   });
-
+console.log('script successful');
 // const sendEmailButton = $(`<button>Get Superb Dresses </button>`).css({
 //   "text-align": "center",
 //   "vertical-align": "middle",
@@ -146,70 +146,70 @@ $("#sendemailbutton").click(() => {
 
 
 //  Best Seller APP
-const makeApp = (products) => {
-  const bestSellerContainer = $(
-    `<div style="overflow-y: scroll;">
-            <h3>Our Best Sellers</h3>
-            ${products
-              .map((item) => {
-                return `
-                <a href="/products/${item.handle}" style="display: flex; align-items: center; padding: 20px 10px; border-top: 1px solid black;">
-                    <img src=${item.images[0].originalSrc} style="width: 75px;" />
-                    <div style="display: flex; justify-content: space-between; align-items: start; width: 100%;">
-                        <p style="padding: 0 10px;">${item.title}</p>
-                        <p>${item.variants[0].price}</p>
-                    </div>
-                </a>
-                `;
-              })
-              .join("")}
-        </div>`
-  ).css({
-    position: "fixed",
-    "background-color": "#ffffff",
-    padding: "10px",
-    border: "1px solid black",
-    bottom: "80px",
-    right: "25px",
-    height: "400px",
-    width: "350px",
-    display: "none",
-    "z-index": 1,
-  });
+// const makeApp = (products) => {
+//   const bestSellerContainer = $(
+//     `<div style="overflow-y: scroll;">
+//             <h3>Our Best Sellers</h3>
+//             ${products
+//               .map((item) => {
+//                 return `
+//                 <a href="/products/${item.handle}" style="display: flex; align-items: center; padding: 20px 10px; border-top: 1px solid black;">
+//                     <img src=${item.images[0].originalSrc} style="width: 75px;" />
+//                     <div style="display: flex; justify-content: space-between; align-items: start; width: 100%;">
+//                         <p style="padding: 0 10px;">${item.title}</p>
+//                         <p>${item.variants[0].price}</p>
+//                     </div>
+//                 </a>
+//                 `;
+//               })
+//               .join("")}
+//         </div>`
+//   ).css({
+//     position: "fixed",
+//     "background-color": "#ffffff",
+//     padding: "10px",
+//     border: "1px solid black",
+//     bottom: "80px",
+//     right: "25px",
+//     height: "400px",
+//     width: "350px",
+//     display: "none",
+//     "z-index": 1,
+//   });
 
-  const bestSellerButton = $("<img/>")
-    .attr(
-      "src",
-      "https://cdn.shopify.com/s/files/1/0325/3174/2765/files/bestseller-button-trans.png?v=1584741923"
-    )
-    .css({
-      position: "fixed",
-      width: "150px",
-      bottom: "20px",
-      right: "20px",
-      cursor: "pointer",
-      "z-index": 1,
-    });
+//   const bestSellerButton = $("<img/>")
+//     .attr(
+//       "src",
+//       "https://cdn.shopify.com/s/files/1/0325/3174/2765/files/bestseller-button-trans.png?v=1584741923"
+//     )
+//     .css({
+//       position: "fixed",
+//       width: "150px",
+//       bottom: "20px",
+//       right: "20px",
+//       cursor: "pointer",
+//       "z-index": 1,
+//     });
 
-  body.append(bestSellerButton);
-  body.append(bestSellerContainer);
+//   body.append(bestSellerButton);
+//   body.append(bestSellerContainer);
 
-  bestSellerButton.click(() => {
-    bestSellerContainer.slideToggle();
-  });
-};
+//   bestSellerButton.click(() => {
+//     bestSellerContainer.slideToggle();
+//   });
+// };
 // https://storefrontify.herokuapp.com/auth?shop=ambraee-dev1.myshopify.com
 // for cors issue add https://cors-anywhere.herokuapp.com/ below with https://storefrontify.herokuapp.com
-fetch(
-  "https://cors-anywhere.herokuapp.com/https://storefrontify.herokuapp.com/api/products?shop=ambraee-dev1.myshopify.com"
-)
-  .then((res) => {
-    res.json()
-    console.log(res);
-  })
-  .then((data) => {
-    makeApp(data.data);
-    // makeHeader(data.data);
-    console.log(data);
-  })
-  .catch((error) => console.log(error));
+// fetch(
+//   "https://cors-anywhere.herokuapp.com/https://storefrontify.herokuapp.com/api/products?shop=ambraee-dev1.myshopify.com"
+// )
+//   .then((res) => {
+//     res.json()
+//     console.log(res);
+//   })
+//   .then((data) => {
+//     makeApp(data.data);
+//     // makeHeader(data.data);
+//     console.log(data);
+//   })
+//   .catch((error) => console.log(error));
