640c8bc3acfedb6566c02171d62b08ea542fb828
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-23 22:55:58 +0530

    add headers in serer.js

--- server.js
+++ server.js
@@ -81,6 +81,9 @@ app.prepare().then(() => {
           secure: true,
           sameSite: 'none'
         });
+        ctx.set('Access-Control-Allow-Origin', '*');
+        ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
         ctx.redirect('/');
         // uncomment below code to add subscription billing page for your app
         //  await getSubscriptionUrl(ctx, accessToken, shop);
