90e6627c12dbef99fc7e8409375c6ef603c741d9
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-24 00:46:37 +0530

    add script

--- pages/PopOver.js
+++ pages/PopOver.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
