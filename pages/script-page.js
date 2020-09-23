39e8b0af1c8339dbea04a250b695dfe869a151f4
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-23 16:59:01 +0530

    Display Scope Selection

--- pages/script-page.js
+++ pages/script-page.js
@@ -10,6 +10,7 @@ import {
   Spinner,
   Select,
 } from "@shopify/polaris";
+import { useState } from "react";
 
 const CREATE_SCRIPT_TAG = gql`
   mutation scriptTagCreate($input: ScriptTagInput!) {
