6766fb77ad29125065a58e27c3bc10eaf85df5fd
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-26 15:56:57 +0530

    add card

--- pages/widgets.js
+++ pages/widgets.js
@@ -1,16 +1,20 @@
 import React from "react";
 import PopupWidget from "./PopupWidget";
-import { Page, Layout } from "@shopify/polaris";
+import { Page, Layout, Card } from "@shopify/polaris";
 
 const widgets = () => {
   return (
     <Page title="Create a widget for Your Store">
       <Layout>
-        <Layout.Section secondary>
-          <PopupWidget />
+        <Layout.Section secondary title="Pop Up Widget">
+          <Card>
+            <PopupWidget />
+          </Card>
         </Layout.Section>
-        <Layout.Section secondary>
-          <PopupWidget />
+        <Layout.Section secondary title="Seller Button Widget">
+          <Card>
+            <PopupWidget />
+          </Card>
         </Layout.Section>
       </Layout>
     </Page>
