b3229c2dc19fde705be700fc40148dfa474de0b3
Rishabh <rishabh.sisodiya4@gmail.com>
2020-09-26 16:23:28 +0530

    add tabs on widgets page

--- pages/widgets.js
+++ pages/widgets.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState , useCallback} from "react";
 import PopupWidget from "./PopupWidget";
-import { Page, Layout, Card } from "@shopify/polaris";
+import { Page, Layout, Card, Tabs } from "@shopify/polaris";
 
 const widgets = () => {
+    const [selected, setSelected] = useState(0);
+
+  const handleTabChange = useCallback(
+    (selectedTabIndex) => setSelected(selectedTabIndex),
+    [],
+  );
+
+  const tabs = [
+    {
+      id: 'popup',
+      content: 'Popup Widget',
+      accessibilityLabel: 'Popup Widget',
+      panelID: 'popup',
+    },
+    {
+      id: 'seller',
+      content: 'Seller Button Widget',
+      panelID: 'seller',
+    },
+  ];
+  let selectedWidget;
+//   if (selected == 0) {
+      selectedWidget = (<PopupWidget />);
+//   }
   return (
     <Page title="Create a widget for Your Store">
-      <Layout>
-        <Layout.Section secondary title="Pop Up Widget">
-          <Card>
-            <PopupWidget />
-          </Card>
-        </Layout.Section>
-        <Layout.Section secondary title="Seller Button Widget">
-          <Card>
-            <PopupWidget />
-          </Card>
-        </Layout.Section>
-      </Layout>
+      <Card>
+        <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
+          <Card.Section title={tabs[selected].content}>
+            {selectedWidget}
+          </Card.Section>
+        </Tabs>
+      </Card>
     </Page>
   );
 };
