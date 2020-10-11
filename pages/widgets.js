4697405f9c5995ed4ad685af8fecb81642a1666e
Rishabh <rishabh.sisodiya4@gmail.com>
2020-10-11 12:41:28 +0530

    Get Shop data

--- pages/widgets.js
+++ pages/widgets.js
@@ -1,54 +1,65 @@
-import React, { useState , useCallback} from "react";
+import React, { useState, useCallback } from "react";
 import PopupWidget from "./PopupWidget";
-import { Card, Tabs } from "@shopify/polaris";
+import { Card, Spinner, Tabs } from "@shopify/polaris";
 import SellerButtonWidget from "./SellerButtonWidget";
-import gql from 'graphql-tag';
-import { useQuery } from '@apollo/react-hooks';
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 
 const GET_SHOP = gql`
-query getShop{
-  shop{
-    url
+  query getShop {
+    shop {
+      url
+    }
   }
-}
 `;
 
 const widgets = () => {
   const { loading, error, data } = useQuery(GET_SHOP);
-  console.log('Shop Data:',data);
+  console.log("Shop Data:", data);
   const [selected, setSelected] = useState(0);
 
   const handleTabChange = useCallback(
     (selectedTabIndex) => setSelected(selectedTabIndex),
-    [],
+    []
   );
 
   const tabs = [
     {
-      id: 'popup',
-      content: 'Popup Widget',
-      accessibilityLabel: 'Popup Widget',
-      panelID: 'popup',
+      id: "popup",
+      content: "Popup Widget",
+      accessibilityLabel: "Popup Widget",
+      panelID: "popup",
     },
     {
-      id: 'seller',
-      content: 'Seller Button Widget',
-      panelID: 'seller',
+      id: "seller",
+      content: "Seller Button Widget",
+      panelID: "seller",
     },
   ];
+  if (loading) {
+    return <Spinner />;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+  let shopUrl;
+  if (data){
+    shopUrl= data.shop.url
+  }
   let selectedWidget;
   if (selected == 0) {
-      selectedWidget = (<PopupWidget shopUrl={data.shop.url}/>);
+    selectedWidget = <PopupWidget shop={shopUrl} />;
   }
-  if(selected ==1){
-      selectedWidget= (<SellerButtonWidget shopUrl={data.shop.url}/>);
+  if (selected == 1) {
+    selectedWidget = <SellerButtonWidget shop={shopUrl} />;
   }
+  
   return (
-      <Card>
-        <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
-            {selectedWidget}
-        </Tabs>
-      </Card>
+    <Card>
+      <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
+        {selectedWidget}
+      </Tabs>
+    </Card>
   );
 };
 
